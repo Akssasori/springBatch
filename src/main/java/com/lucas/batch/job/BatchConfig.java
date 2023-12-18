7ae608695d30362060c4a44d1714ba57133c8f42
Lucas <diniz3003@gmail.com>
2023-12-18 09:58:53 -0300

    refactor spring batch

---
+++ src/main/java/com/lucas/batch/job/BatchConfig.java
@@ -0,0 +1,26 @@
+package com.lucas.batch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    public Job imprimiOlaJob(Step imprimeOlaStep) {
+        return jobBuilderFactory
+                .get("imprimiOlaJob")
+                .start(imprimeOlaStep)
+                .build();
+    }
+
+}
